27b9967095347291e372abeaae7c343cad1a8794
Csongor Bakos <cbakos@voice.ai>
2023-05-15 15:01:15 +0300

    remove register route, add auth language lines

---
+++ resources/lang/hu/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Nem megfelelő bejelentkezési adatok.',
+    'password' => 'A megadott jelszó nem érvényes.',
+    'throttle' => 'Túl sok próbálkozás. Próbálja újra :seconds másodperc múlva.',
+
+];
---
+++ resources/lang/hu/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Sikeres jelszócsere!',
+    'sent' => 'Jelszócsere link elküldve!',
+    'throttled' => 'Kérem várjon mielőtt újra próbálkozik.',
+    'token' => 'Érvénytelen jelszócsere token.',
+    'user' => "Nincs ilyen felhasználó.",
+
+];
